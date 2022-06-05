f39480a15686e6689e56810234e10167ad094b7d
edivaner <edivanerfernandes18@gmail.com>
2022-06-05 18:36:21 -0300

    iniciando repositorio

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <footer>
+        <p>Make Your burger &copy; 2022</p>
+    </footer>
+  </div>
+</template>
+
+
+<script>
+export default {
+    name:"FooterComponent"
+}
+</script>
+
+<style scoped>
+  footer{
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #FCBA03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    bottom: 0;
+    width: 100%;
+  }
+</style>
---
+++ src/views/Pedidos.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="main-container">
+    <h1>Todos os Pedidos</h1>
+  </div>
+</template>
